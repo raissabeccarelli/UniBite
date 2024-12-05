71307ebebe124112040e426c092106747043ca22
GiacomoLocatelli <126594710+GiacomoLocatelli@users.noreply.github.com>
2024-12-05 10:53:14 +0100

    prova

--- unibite/src/main/java/gui/Application.java
+++ unibite/src/main/java/gui/Application.java
@@ -10,6 +10,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  *
  * Use the @PWA annotation make the application installable on phones, tablets
  * and some desktop browsers.
+ * 
  *
  */
 @SpringBootApplication
