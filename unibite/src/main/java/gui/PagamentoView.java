8959b825870144ad338c5134226ddb7c7accfa4d
raissabeccarelli <126601098+raissabeccarelli@users.noreply.github.com>
2025-01-25 15:44:01 +0100

    commenti a PagamentoView e PortalepeersonaleView

--- unibite/src/main/java/gui/PagamentoView.java
+++ unibite/src/main/java/gui/PagamentoView.java
@@ -30,86 +30,97 @@ import model.Connessione;
 public class PagamentoView extends Composite<VerticalLayout> {
 
 	public PagamentoView() {
+		//prende lo stuudente che gli viene ppassato dalla view precedente
 		StudenteDocente sd = (StudenteDocente) VaadinSession.getCurrent().getAttribute("SDC");	
+		//prende il carrello che gli viene passato dalla view precedente
 		Carrello c = (Carrello) VaadinSession.getCurrent().getAttribute("CARRELLO");	
+		//crea le intestazioni
 		H1 h1 = new H1();
+		H4 h4 = new H4();
+		H4 h6 = new H4();
+		//crea il separatore
 		Hr hr = new Hr();
-		Paragraph textSmall = new Paragraph();
+		Paragraph textSmall = new Paragraph(); //crea un paragrafo di testo
+		//crea i vari layout
 		VerticalLayout layoutColumn2 = new VerticalLayout();
 		HorizontalLayout layoutRow = new HorizontalLayout();
 		HorizontalLayout layoutRow2 = new HorizontalLayout();
 		HorizontalLayout layoutRow4 = new HorizontalLayout();
-		H4 h4 = new H4();
-		H4 h6 = new H4();
 		HorizontalLayout layoutRow3 = new HorizontalLayout();
-		Button buttonPrimary = new Button();
 		HorizontalLayout layoutRow5 = new HorizontalLayout();
+		//crea i bottoni
+		Button buttonPrimary = new Button();
 		Button buttonPrimary2 = new Button();
-		getContent().setWidth("100%");
+		
+		getContent().setWidth("100%"); //imposta la larghezza al massimo
+		//indica agli elementi quanto espandersi
 		String style = "flex-grow";
+		
 		getContent().getStyle().set(style, "1");
-		h1.setText("PAGAMENTO");
-		getContent().setAlignSelf(FlexComponent.Alignment.CENTER, h1);
+		h1.setText("PAGAMENTO"); //setta il testo dell'intestazione
+		getContent().setAlignSelf(FlexComponent.Alignment.CENTER, h1); //allinea al centro
+		//imosta la dimensione minima dei contenitori
 		String width = "max-content";
-		h1.setWidth(width);
+		h1.setWidth(width); //imposta la dimensione massima
+		//setta il testo
 		textSmall.setText("Scegli se pagare in cassa oppure se addebitare il pranzo sul tuo conto virtuale");
-		getContent().setAlignSelf(FlexComponent.Alignment.CENTER, textSmall);
-		textSmall.setWidth(width);
+		getContent().setAlignSelf(FlexComponent.Alignment.CENTER, textSmall); //allinea al centro il testo sopra
+		textSmall.setWidth(width); //imposta la dimensione massima
 		textSmall.getStyle().set("font-size", "var(--lumo-font-size-xs)");
-		layoutColumn2.setWidthFull();
+		layoutColumn2.setWidthFull(); //imposta la larghezza al massimo
 		getContent().setFlexGrow(1.0, layoutColumn2);
-		layoutColumn2.setWidth("100%");
 		layoutColumn2.getStyle().set(style, "1");
-		layoutRow.setWidthFull();
+		layoutRow.setWidthFull(); //imposta la larghezza al massimo
 		layoutColumn2.setFlexGrow(1.0, layoutRow);
 		layoutRow.addClassName(Gap.MEDIUM);
-		layoutRow.setWidth("100%");
 		layoutRow.getStyle().set(style, "1");
-		layoutRow2.setHeightFull();
+		layoutRow2.setHeightFull(); //imposta l'altezza al massimo
 		layoutRow.setFlexGrow(1.0, layoutRow2);
 		layoutRow2.addClassName(Gap.MEDIUM);
-		layoutRow2.setWidth("100%");
+		layoutRow2.setWidth("100%"); //imposta la larghezza al massimo
 		layoutRow2.getStyle().set(style, "1");
-		layoutRow2.setAlignItems(Alignment.CENTER);
-		layoutRow2.setJustifyContentMode(JustifyContentMode.CENTER);
-		layoutRow4.setHeightFull();
+		layoutRow2.setAlignItems(Alignment.CENTER); //allinea gli elementi al centro orizzontalemnte
+		layoutRow2.setJustifyContentMode(JustifyContentMode.CENTER); //allinea gli elementi al centro verticalmente
+		layoutRow4.setHeightFull(); //imposta l'altezza al massimo
 		layoutRow2.setFlexGrow(1.0, layoutRow4);
 		layoutRow4.addClassName(Gap.MEDIUM);
-		layoutRow4.setWidth("780px");
+		layoutRow4.setWidth("780px"); //imposta la larghezza
 		layoutRow4.getStyle().set(style, "1");
-		h4.setText("Credito virtuale:");
-		layoutRow2.setAlignSelf(FlexComponent.Alignment.CENTER, h4);
-		h4.setWidth(width);
-		h6.setText(sd.getContoVirtuale(sd.getMatricola())+"€");
-		layoutRow2.setAlignSelf(FlexComponent.Alignment.CENTER, h6);
-		h6.setWidth(width);
-		layoutRow3.setWidthFull();
+		h4.setText("Credito virtuale:"); //setta il testo dell'intestazione
+		layoutRow2.setAlignSelf(FlexComponent.Alignment.CENTER, h4); //centra l'intestazione
+		h4.setWidth(width); //imposta la dimensione massima
+		h6.setText(sd.getContoVirtuale(sd.getMatricola())+"€"); //setta il testo dell'intestazione utilizzando un metodo
+		layoutRow2.setAlignSelf(FlexComponent.Alignment.CENTER, h6); //allinea centralmente
+		h6.setWidth(width); //imposta la larghezza al massimo
+		layoutRow3.setWidthFull(); //imposta la larghezza al massimo
 		layoutColumn2.setFlexGrow(1.0, layoutRow3);
 		layoutRow3.addClassName(Gap.MEDIUM);
-		layoutRow3.setWidth("100%");
 		layoutRow3.getStyle().set(style, "1");
-		layoutRow3.setAlignItems(Alignment.CENTER);
-		layoutRow3.setJustifyContentMode(JustifyContentMode.CENTER);
-		buttonPrimary.setText("PAGA IN CASSA");
-		layoutRow3.setAlignSelf(FlexComponent.Alignment.CENTER, buttonPrimary);
-		String dimensioneBottone= "300px";
+		layoutRow3.setAlignItems(Alignment.CENTER); //allinea gli elementi inn centro orizzontalmente
+		layoutRow3.setJustifyContentMode(JustifyContentMode.CENTER); //allinea gli elementi in centro verticalmente
+		buttonPrimary.setText("PAGA IN CASSA"); //setta il testo del bottone
+		layoutRow3.setAlignSelf(FlexComponent.Alignment.CENTER, buttonPrimary); //allinea al centro il bottone
+		
+		String dimensioneBottone= "300px"; //setta le dimensioni del bottone
 		buttonPrimary.setWidth(dimensioneBottone);
 		buttonPrimary.setHeight(dimensioneBottone);
-		buttonPrimary.addThemeVariants(ButtonVariant.LUMO_PRIMARY);
-		layoutRow5.setHeightFull();
+		buttonPrimary.addThemeVariants(ButtonVariant.LUMO_PRIMARY); //aggiunge il bottone ai bottoni primari
+		layoutRow5.setHeightFull(); //imposta l'altezza al massimo
 		layoutRow3.setFlexGrow(1.0, layoutRow5);
 		layoutRow5.addClassName(Gap.MEDIUM);
-		layoutRow5.setWidth("200px");
+		layoutRow5.setWidth("200px"); //imposta la larghezza 
 		layoutRow5.getStyle().set(style, "1");
-		buttonPrimary2.setText("PAGA COL CREDITO VIRTUALE");
-		layoutRow3.setAlignSelf(FlexComponent.Alignment.CENTER, buttonPrimary2);
-		buttonPrimary2.setWidth(dimensioneBottone);
+		buttonPrimary2.setText("PAGA COL CREDITO VIRTUALE"); //setta il testo del bottone
+		layoutRow3.setAlignSelf(FlexComponent.Alignment.CENTER, buttonPrimary2); //centra il bottone nel layout
+		buttonPrimary2.setWidth(dimensioneBottone); 
 		buttonPrimary2.setHeight(dimensioneBottone);
-		buttonPrimary2.addThemeVariants(ButtonVariant.LUMO_PRIMARY);
+		buttonPrimary2.addThemeVariants(ButtonVariant.LUMO_PRIMARY); //aggiunge il bottone ai bottoni primari
+		//aggiunge alla view
 		getContent().add(h1);
 		getContent().add(hr);
 		getContent().add(textSmall);
 		getContent().add(layoutColumn2);
+		//aggiunge ai layout
 		layoutColumn2.add(layoutRow);
 		layoutRow.add(layoutRow2);
 		layoutRow2.add(h4);
@@ -119,9 +130,9 @@ public class PagamentoView extends Composite<VerticalLayout> {
 		layoutRow3.add(buttonPrimary2);
 		
 		
-		//vai in cassa
+		//bottone vai in cassa
 		buttonPrimary.addClickListener(event -> UI.getCurrent().navigate("my-view8"));
-		//pagamento virtuale
+		//bottone pagamento virtuale
 		buttonPrimary2.addClickListener(event -> {
 			Connessione connessione = Connessione.getInstance();
 			DSLContext dslContext = connessione.getDslContext();
--- unibite/src/main/java/gui/PortalepersonaleView.java
+++ unibite/src/main/java/gui/PortalepersonaleView.java
@@ -33,82 +33,92 @@ public class PortalepersonaleView extends Composite<VerticalLayout> {
 		image.setWidth("100px"); // Imposta la larghezza
 		image.setHeight("100px"); // Imposta l'altezza
 		HorizontalLayout layoutRow2 = new HorizontalLayout();
+		
+		//mette un intestazione
 		H3 h3 = new H3();
-		Button buttonPrimary = new Button();
+		
+		Button buttonPrimary = new Button(); //creazione bottone poi utilizzato per il logout
 		Paragraph textSmall = new Paragraph();
 		HorizontalLayout layoutRow3 = new HorizontalLayout();
-		Button buttonPrimary2 = new Button();
+		Button buttonPrimary2 = new Button(); //creazione bottone poi utilizzato per aggiungere nuove ricette
 		HorizontalLayout layoutRow6 = new HorizontalLayout();
 		VerticalLayout layoutColumn2 = new VerticalLayout();
 		HorizontalLayout layoutRow4 = new HorizontalLayout();
-
-		List<Record2<String, Integer>> piatti = Piatto.cercaPiatti();
-		Grid<Record2<String, Integer>> grid = new Grid<>();
 		
-	
-        
+		//crea una lista con due attributi (nome del piatto e quantità disponibili) presi dal database tramite il 
+		//metodo cerca piatti, da mostrare a schermo
+		List<Record2<String, Integer>> piatti = Piatto.cercaPiatti();
+		//creazione di una griglia su cui poi verrà proiettata la lista sopra
+		Grid<Record2<String, Integer>> grid = new Grid<>();
         
 
 		HorizontalLayout layoutRow5 = new HorizontalLayout();
 		VerticalLayout layoutColumn3 = new VerticalLayout();
+		//field per poter inserire le nuove quantità di porzioni disponibili, come interi
 		IntegerField numberField = new IntegerField();
+		//bottone che prende le quantità inserite sopra e le inserisce nel db
 		Button buttonPrimary3 = new Button();
 		VerticalLayout layoutColumn4 = new VerticalLayout();
+		//bottone "Elimina piatto", per settare le porzioni di un piatto a 0
 		Button buttonPrimary4 = new Button();
 		getContent().setWidth("100%");
+		
+		//variabile che indica quanto devono espandersi gli elementi
 		String style = "flex-grow";
+		//variabile che imposta la dimensione minima del contenitore
 		String dim = "min-content";
+		
 		getContent().getStyle().set(style, "1");
-		layoutRow.setWidthFull();
-		getContent().setFlexGrow(1.0, layoutRow);
+		layoutRow.setWidthFull(); //imposta la larghezza al massimo
+		getContent().setFlexGrow(1.0, layoutRow); //fa in modo che si espanda all'interno di layoutRow
 		layoutRow.addClassName(Gap.MEDIUM);
 		layoutRow.setWidth("100%");
-		layoutRow.setHeight(dim);
-		layoutRow.setAlignItems(Alignment.CENTER);
-		layoutRow.setJustifyContentMode(JustifyContentMode.CENTER);
-
-		layoutRow2.setHeightFull();
+		layoutRow.setHeight(dim); //imposta l'altezza al minimo
+		layoutRow.setAlignItems(Alignment.CENTER); //allinea verticalmente al centro
+		layoutRow.setJustifyContentMode(JustifyContentMode.CENTER); //allinea orizzontalmente al centro
+		
+		layoutRow2.setHeightFull(); //imposta layoutRow2 alla massima altezza
 		layoutRow.setFlexGrow(1.0, layoutRow2);
 		layoutRow2.addClassName(Gap.MEDIUM);
-		layoutRow2.setWidth("100%");
-		layoutRow2.setHeight(dim);
-		h3.setText("MODIFICA IL MENU' DEL GIORNO");
-		h3.setWidth("max-content");
-		buttonPrimary.setText("LOGOUT");
-		buttonPrimary.setWidth(dim);
-		buttonPrimary.addThemeVariants(ButtonVariant.LUMO_PRIMARY);
+		layoutRow2.setWidth("100%"); //imposta alla massima larghezza
+		layoutRow2.setHeight(dim); //imposta alla minima altezza
+		h3.setText("MODIFICA IL MENU' DEL GIORNO"); //setta il testo tra () nell'intestazione
+		h3.setWidth("max-content"); //imposta la larghezza del testo al massimo
+		buttonPrimary.setText("LOGOUT"); //setta il testo sul bottone
+		buttonPrimary.setWidth(dim); //imposta la dimensione del bottone al minimo 
+		buttonPrimary.addThemeVariants(ButtonVariant.LUMO_PRIMARY); //rende il bottone un pulsante primario
+		//setta il testo da vedere a schermo
 		textSmall.setText(
 				"Per aggiungere un piatto al menù, selezionalo e clicca \"Aggiungi quantità\". "
 				+ "Per eliminarlo dal menù, selezionalo e clicca \"Elimina piatto\". "
 				+ "Per aggiungere una ricetta seleziona \"Aggiungi ricetta\". "
 				+ "Ricorda che devono essere disponibili almeno 3 piatti diversi per ogni categoria");
-		textSmall.setWidth("100%");
-		textSmall.getStyle().set("font-size", "var(--lumo-font-size-xs)");
-		layoutRow3.setWidthFull();
+		textSmall.setWidth("100%"); //imposta la largezza del testo al massimo
+		textSmall.getStyle().set("font-size", "var(--lumo-font-size-xs)"); 
+		layoutRow3.setWidthFull(); //imposta la larghezza al massimo
 		getContent().setFlexGrow(1.0, layoutRow3);
 		layoutRow3.addClassName(Gap.MEDIUM);
-		layoutRow3.setWidth("100%");
-		layoutRow3.setHeight(dim);
-		layoutRow3.setAlignItems(Alignment.START);
-		layoutRow3.setJustifyContentMode(JustifyContentMode.START);
-		buttonPrimary2.setText("Aggiungi ricetta");
-		buttonPrimary2.setWidth(dim);
-		buttonPrimary2.addThemeVariants(ButtonVariant.LUMO_PRIMARY);
-		layoutRow6.setHeightFull();
+		layoutRow3.setHeight(dim); //imposta altezza al minimo
+		layoutRow3.setAlignItems(Alignment.START); //allinea gli elementi di layouutRow3 all'inizio verticalmente
+		layoutRow3.setJustifyContentMode(JustifyContentMode.START); //allinea gli elementi all'inizio orizzontalmente
+		buttonPrimary2.setText("Aggiungi ricetta"); //setta il testo dell bottone
+		buttonPrimary2.setWidth(dim); //setta la dimensione del bottone al minimo
+		buttonPrimary2.addThemeVariants(ButtonVariant.LUMO_PRIMARY); //rende il bottone un bottone primario
+		layoutRow6.setHeightFull(); //imposta la larghezza al massimo
 		layoutRow3.setFlexGrow(1.0, layoutRow6);
 		layoutRow6.addClassName(Gap.MEDIUM);
 		layoutRow6.getStyle().set(style, "1");
 		layoutRow6.getStyle().set(style, "1");
-		layoutColumn2.setWidthFull();
+		layoutColumn2.setWidthFull(); //imposta la larghezza al massimo
 		getContent().setFlexGrow(1.0, layoutColumn2);
-		layoutColumn2.setWidth("100%");
 		layoutColumn2.getStyle().set(style, "1");
-		layoutRow4.setWidthFull();
+		layoutRow4.setWidthFull(); //imposta la larghezza al massimo
 		layoutColumn2.setFlexGrow(1.0, layoutRow4);
 		layoutRow4.addClassName(Gap.MEDIUM);
 		layoutRow4.setWidth("100%");
 		layoutRow4.getStyle().set(style, "1");
 
+		//imposta la larghezza della griglia
 		grid.setWidth("5000px");
 		// Imposta le colonne della Grid
 		grid.addColumn(record -> record.value1()) // NOME
@@ -120,39 +130,42 @@ public class PortalepersonaleView extends Composite<VerticalLayout> {
 		// Imposta i dati da visualizzare nella Grid
 		grid.setItems(piatti);
 
-		layoutRow5.setHeightFull();
+		layoutRow5.setHeightFull(); //imposta l'altezza al massimo
 		layoutRow4.setFlexGrow(1.0, layoutRow5);
 		layoutRow5.addClassName(Gap.MEDIUM);
-		layoutRow5.setWidth("100%");
+		layoutRow5.setWidth("100%"); //imposta la larghezza al massimo
 		layoutRow5.getStyle().set(style, "1");
-		layoutColumn3.setHeightFull();
+		layoutColumn3.setHeightFull(); //imposta l'altezza al massimo
 		layoutRow5.setFlexGrow(1.0, layoutColumn3);
-		layoutColumn3.setWidth("100%");
+		layoutColumn3.setWidth("100%"); //imposta la larghezza al massimo
 		layoutColumn3.getStyle().set(style, "1");
-		layoutColumn3.setJustifyContentMode(JustifyContentMode.START);
-		layoutColumn3.setAlignItems(Alignment.CENTER);
-		numberField.setLabel("Inserisci porzioni");
-		numberField.setWidth("179px");
-		buttonPrimary3.setText("Imposta il numero di porzioni");
-		buttonPrimary3.setWidth(dim);
-		buttonPrimary3.addThemeVariants(ButtonVariant.LUMO_PRIMARY);
-		layoutColumn4.setWidthFull();
+		layoutColumn3.setJustifyContentMode(JustifyContentMode.START); //mette gli elementi di layoutColumn3 all'inizio orizzontalmente
+		layoutColumn3.setAlignItems(Alignment.CENTER); //mette gli elementi in centro verticalmente
+		numberField.setLabel("Inserisci porzioni"); //setta il titolo del field
+		numberField.setWidth("179px"); //imposta la dimensione
+		buttonPrimary3.setText("Imposta il numero di porzioni"); //imposta il testo del bottone
+		buttonPrimary3.setWidth(dim); //imposta la diemnsione del bottone
+		buttonPrimary3.addThemeVariants(ButtonVariant.LUMO_PRIMARY); //imposta il bottone come bottone primario
+		layoutColumn4.setWidthFull(); //imposta la larghezza al massimo
 		layoutColumn3.setFlexGrow(1.0, layoutColumn4);
-		layoutColumn4.setWidth("100%");
-		layoutColumn4.setHeight("20px");
-		buttonPrimary4.setText("Elimina piatto ❌");
-		buttonPrimary4.setWidth("179px");
-		buttonPrimary4.addThemeVariants(ButtonVariant.LUMO_PRIMARY);
-		getContent().add(layoutRow);
-		layoutRow.add(image);
-		layoutRow.add(layoutRow2);
+		layoutColumn4.setHeight("20px"); //imposta l'altezza
+		buttonPrimary4.setText("Elimina piatto ❌"); //setta il testo del bottone
+		buttonPrimary4.setWidth("179px"); //imposta la diemnsione del bottone
+		buttonPrimary4.addThemeVariants(ButtonVariant.LUMO_PRIMARY); //mette il bottone come bottone primario
+		getContent().add(layoutRow); //aggiunge alla vista il layoutRow
+		layoutRow.add(image); //aggiunge al layoutRow l'immagine
+		layoutRow.add(layoutRow2); //aggiunge al layoutRow
 		layoutRow2.add(h3);
 		layoutRow.add(buttonPrimary);
+		//aggiunge alla vista 
 		getContent().add(textSmall);
 		getContent().add(layoutRow3);
+		//aggiunge al layoutRow3
 		layoutRow3.add(buttonPrimary2);
 		layoutRow3.add(layoutRow6);
+		//aggiunge alla vista
 		getContent().add(layoutColumn2);
+		//aggiunge ai vari layout
 		layoutColumn2.add(layoutRow4);
 		layoutRow4.add(grid);
 		layoutRow4.add(layoutRow5);
@@ -162,13 +175,13 @@ public class PortalepersonaleView extends Composite<VerticalLayout> {
 		layoutColumn3.add(layoutColumn4);
 		layoutColumn3.add(buttonPrimary4);
 		
-		
-		
+		//gestisce il click del bottone 
 		buttonPrimary2.addClickListener(event -> UI.getCurrent().navigate("my-view10"));
 		buttonPrimary.addClickListener(event -> {
 			
 			UI.getCurrent().navigate("");
 		});
+		
 		Personale p = new Personale();
 		buttonPrimary3.addClickListener(event -> {
 			p.aggiungiPorzioni(grid.getSelectedItems(), numberField.getValue());
